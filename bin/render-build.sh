3d48205cd5dab572ef9eb079b6a4955732481c6b
Mahima <mahima.c@pixoatic.com>
2024-10-23 19:03:02 +0530

    webpacker issue

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 bundle install
 bundle exec echo yes | bundle exec npm install -D webpack-cli
-bundle exec rails webpacker:install
+# bundle exec rails webpacker:install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
