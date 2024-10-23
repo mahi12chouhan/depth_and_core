7ac974007a57122737af95801dd8091b53cc5749
Mahima <mahima.c@pixoatic.com>
2024-10-23 18:52:11 +0530

    render changes

---
+++ bin/render-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec echo yes | bundle exec npm install -D webpack-cli
+bundle exec rails webpacker:install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+bundle exec rails db:seed RAILS_ENV=production
